b7d7710f330be9dee6f4a258888b50b8aa30d6ca
Gutok66 <akesslerpires@gmail.com>
2022-12-11 14:35:03 -0300

    componentes gutok

---
+++ header/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export default function Header() {
+
+    return (
+        <div className="Header">
+            <div className="Fields">
+                <a href="#banner">HOME</a>
+                <a href="#">PRODUTOS</a>
+                <a href="#">SOBRE</a>
+            </div>
+        </div>
+    )
+}
