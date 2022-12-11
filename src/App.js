fafc5a2703c5e3c03855e7c9f692717b07c9ea76
vitorholiveira <vitorh.magnus.oliveira@gmail.com>
2022-12-11 19:21:53 -0300

    Instagram e Footer
    Implementação parcial do instagram e do footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Instagram from './components/instagram/Instagram'
 
 function App () {
   return (
-    <Header />
+    <div>
+      <Instagram/>
+      <Footer/>
+    </div>
   );
 }
 
