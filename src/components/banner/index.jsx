c3fa5dfad3518c5ce95cdb698b3ae1542a2dc9a5
Gutok66 <akesslerpires@gmail.com>
2022-12-11 22:17:28 -0300

    degra

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -4,11 +4,10 @@ export default function Banner() {
 
     return (
         <div id="banner" className="Banner">
-            <div className="texto">
-                Promoção de Natal na DecorHouse!
-            </div>
             <div className="degra">
-                <br/>
+                <div className="texto">
+                    Promoção de Natal na DecorHouse!
+                </div>
             </div>
 
 
