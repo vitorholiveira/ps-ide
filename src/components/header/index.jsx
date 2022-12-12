b3aa8e8011a3c53ea67bc9c69ef6db2acd61cca1
Gutok66 <akesslerpires@gmail.com>
2022-12-12 14:45:39 -0300

    ajuste

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,12 +3,12 @@ import './style.css';
 export default function Header() {
 
     return (
-        <div id="home"className="Header">
+        <header id="home"className="Header">
             <div className="Fields">
                 <a href="#home"><b>HOME</b></a>
                 <a href="#"><b>PRODUTOS</b></a>
                 <a href="#sobre"><b>SOBRE</b></a>
             </div>
-        </div>
+        </header>
     )
 }
