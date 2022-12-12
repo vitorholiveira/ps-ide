4e7c33f5fb2309b2939078b1e3aff3c510c6bbbf
vitorholiveira <vitorh.magnus.oliveira@gmail.com>
2022-12-12 11:06:49 -0300

    Instagram e Footer
    Atualização da qualidade das imagens e ajuste na proporcao do footer e instagram.

--- src/components/instagram/Instagram.js
+++ src/components/instagram/Instagram.js
@@ -6,20 +6,25 @@ import anonovo from '../../img/anonovo.png'
 function Instagram() {
     return (
         <div className="container">
+<<<<<<< Updated upstream
             <div className="children">
                 SIGA-NOS NO INSTAGRAM
+=======
+            <div id="insta"className="children">
+                <p><b>SIGA-NOS NO INSTAGRAM</b></p>
+>>>>>>> Stashed changes
                 <ul className="fotos">
                     <li>
                         <img src={copa} alt="DECOR" />
-                        <p><b>DecorHouse</b> a DecorHouse já está <br/>em clima de copa do mundo ...</p>
+                        <p><b>DecorHouse</b> a DecorHouse já está em clima de copa do mundo ...</p>
                     </li>
                     <li>
                         <img src={natal} alt="natal" />
-                        <p><b>DecorHouse</b> já está pensando em <br/>como vai decorar a casa para ...</p>
+                        <p><b>DecorHouse</b> já está pensando em como vai decorar a casa para ...</p>
                     </li>
                     <li>
                         <img src={anonovo} alt="anonovo" />
-                        <p><b>DecorHouse</b> visite nossas lojas <br/>para comprar sua decoração de ...</p>
+                        <p><b>DecorHouse</b> visite nossas lojas para comprar sua decoração de ...</p>
                     </li>
                 </ul>
             </div>
