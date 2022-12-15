073d0d2f26a09bdf7d464c8221d24c49b2c27bf1
thiagosc121 <thiagosc142@gmail.com>
2022-12-15 19:06:37 -0300

    About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/header'
 import Banner from './components/banner'
 import Produtos from './components/produtos'
+import About from './components/about/About'
 import Footer from './components/footer/Footer'
 import Instagram from './components/instagram/Instagram'
 
@@ -12,6 +13,7 @@ function App () {
       <Banner/>
       <Produtos/>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <About/>
       <Instagram/>
       <Footer/>
     </div>
